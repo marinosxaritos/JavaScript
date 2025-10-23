a90aacfa4a1dfa76496443ad3a2da2adf26fbf36
marinosxaritos <marichar8@teiser.gr>
2025-10-23 15:56:46 +0300

    .

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
-document.getElementById("showBtn").addEventListener("click", function() {
-            const userData = localStorage.getItem('userData');
-            const output = document.getElementById("userInfo");
+document.getElementById("showBtn").addEventListener("click", function () {
+  const userData = localStorage.getItem("userData");
+  const output = document.getElementById("userInfo");
 
-            if (userData) {
-                const user = JSON.parse(userData);
-                output.innerHTML = `
+  if (userData) {
+    const user = JSON.parse(userData);
+    output.innerHTML = `
                     <p><strong>Email:</strong> ${user.email}</p>
                     <p><strong>Ώρα:</strong> ${user.time}</p>
                     <p><strong>Ημερομηνία:</strong> ${user.date}</p>
                     <p><strong>Τηλέφωνο:</strong> ${user.phone}</p>
                     <p><strong>Κωδικός:</strong> ${user.password}</p>
                 `;
-            } else {
-                output.textContent = "Δεν υπάρχουν αποθηκευμένα δεδομένα.";
-            }
-        });
+  } else {
+    output.textContent = "Δεν υπάρχουν αποθηκευμένα δεδομένα.";
+  }
+});
--- validation.js
+++ validation.js
@@ -1,122 +1,111 @@
-        
-            function maskEmail(email) {
-                let atSignIndex = email.indexOf("@");
-                let emailPart = email.slice(1, atSignIndex -1);
-                let countEmailPart = emailPart.length;
-                let asterisk = "*"
-                return  email.replace(emailPart, asterisk.repeat(countEmailPart))
-            }  
+function maskEmail(email) {
+  let atSignIndex = email.indexOf("@");
+  let emailPart = email.slice(1, atSignIndex - 1);
+  let countEmailPart = emailPart.length;
+  let asterisk = "*";
+  return email.replace(emailPart, asterisk.repeat(countEmailPart));
+}
 
-            let originalEmail = "";
+let originalEmail = "";
 
-            function maskEmailAction() {
-                let emailValue = emailInput.value;
-                if (emailValue.trim() === "") {
-                    alert("Please enter an email first!");
-                    return;
-                }
-                originalEmail = emailValue;
-                emailInput.value = maskEmail(emailValue); 
-            }
+function maskEmailAction() {
+  let emailValue = emailInput.value;
+  if (emailValue.trim() === "") {
+    alert("Please enter an email first!");
+    return;
+  }
+  originalEmail = emailValue;
+  emailInput.value = maskEmail(emailValue);
+}
 
-            const emailInput = document.getElementById("email");
+const emailInput = document.getElementById("email");
 
-            function showHidePassword() {
-                var x = document.getElementById("password");
-                if (x.type === "password") {
-                    x.type = "text";
-                } else {
-                    x.type = "password";
-                }
-            }
-            
-            
-            emailInput.addEventListener("input", (e) => {   
-                e.target.setCustomValidity("");
+function showHidePassword() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 
-                if (!e.target.value.endsWith("@netcompany.com")) {
-                    e.target.setCustomValidity(
-                        "You must use a company email address that ends in @netCompany.com"
-                    );
-                }
-            });
+emailInput.addEventListener("input", (e) => {
+  e.target.setCustomValidity("");
 
+  if (!e.target.value.endsWith("@makeithappen.com")) {
+    e.target.setCustomValidity(
+      "You must use a company email address that ends in @makeithappen.com"
+    );
+  }
+});
 
-            const actualDate = document.getElementById("date");
-            const actual = new Date();
-            actualDate.value = actual.toISOString().split("T")[0]; 
+const actualDate = document.getElementById("date");
+const actual = new Date();
+actualDate.value = actual.toISOString().split("T")[0];
 
-            const dateInput = document.getElementById("date");
-            
-            dateInput.addEventListener("input",(e) => {
-                const selectedDate = new Date(e.target.value);
-                const now = new Date();
+const dateInput = document.getElementById("date");
 
-                if (selectedDate > now) {
-                    e.target.setCustomValidity("You must select a past date");
-                } else {
-                    e.target.setCustomValidity("");
-                }
-            });
+dateInput.addEventListener("input", (e) => {
+  const selectedDate = new Date(e.target.value);
+  const now = new Date();
 
-            const telInput = document.getElementById("phone");
+  if (selectedDate > now) {
+    e.target.setCustomValidity("You must select a past date");
+  } else {
+    e.target.setCustomValidity("");
+  }
+});
 
-            telInput.addEventListener("input", (e) => {   
-                e.target.setCustomValidity("");
+const telInput = document.getElementById("phone");
 
-                if (!e.target.value.startsWith("69") || e.target.value.length !== 8) {
-                    e.target.setCustomValidity(
-                        "Your phone Number must start with 69 and must be at total 8 numbers"
-                    );
-                }
-            });
+telInput.addEventListener("input", (e) => {
+  e.target.setCustomValidity("");
 
-            const passwordInput = document.getElementById("password");
+  if (!e.target.value.startsWith("69") || e.target.value.length !== 8) {
+    e.target.setCustomValidity(
+      "Your phone Number must start with 69 and must be at total 8 numbers"
+    );
+  }
+});
 
-            passwordInput.addEventListener("input", (e) => {   
-                e.target.setCustomValidity("");
+const passwordInput = document.getElementById("password");
 
-                if (e.target.value.length < 6) {
-                    e.target.setCustomValidity(
-                        "You must use a more the 6 characters"
-                    );
-                }
-            });
+passwordInput.addEventListener("input", (e) => {
+  e.target.setCustomValidity("");
 
-            const timeInput = document.getElementById("time");
-            
+  if (e.target.value.length < 6) {
+    e.target.setCustomValidity("You must use a more the 6 characters");
+  }
+});
 
-            document.getElementById("form").addEventListener("submit", function(e) {
-                e.preventDefault();
+const timeInput = document.getElementById("time");
 
-                const user = {
-                    email: originalEmail,
-                    password: passwordInput.value,
-                    time: timeInput.value,
-                    date: dateInput.value,
-                    phone: telInput.value,
-                };
-            
-                const users = JSON.parse(localStorage.getItem('users')) || [];
+document.getElementById("form").addEventListener("submit", function (e) {
+  //console.log(e);
 
-                const exists = users.some(u => u.email === user.email);
-                if (exists) {
-                    alert("email already exists");
-                    return; 
-                }
+  e.preventDefault();
 
-                users.push(user);
-                localStorage.setItem('users', JSON.stringify(users));
-                alert("Επιτυχής εγγραφή!");
+  const user = {
+    email: originalEmail,
+    password: passwordInput.value,
+    time: timeInput.value,
+    date: dateInput.value,
+    phone: telInput.value,
+  };
 
-                const jsonString = JSON.stringify(user, null, 2);
-                console.log(jsonString);
+  const users = JSON.parse(localStorage.getItem("users")) || [];
 
+  const exists = users.some((u) => u.email === user.email);
+  if (exists) {
+    alert("email already exists");
+    return;
+  }
 
-                localStorage.setItem('userData',jsonString);
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
 
-                
-            
-            });
+  const jsonString = JSON.stringify(user, null, 2);
+  console.log(jsonString);
 
-        
+  localStorage.setItem("userData", jsonString);
+});
